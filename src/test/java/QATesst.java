9947c3fc688af452eed476ac20ce66c1d8afede3
Laylet99 <keino698gmail.com>
2020-07-27 19:12:18 +0300

    wq

--- src/test/java/QATesst.java
+++ src/test/java/QATesst.java
@@ -4,36 +4,44 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-
+import java.util.concurrent.TimeUnit;
 
 
 public class QATesst {
+public static final Logger logger = LogManager.getLogger(QATesst.class);
+   private WebDriver driver;
 
-    protected static WebDriver driver;
-    private Logger logger = LogManager.getLogger(QATesst.class);
 
-    @Before
-    public void setUp() {
+    @BeforeClass
+    public static void setUpClass() {
         WebDriverManager.chromedriver().setup();
-        driver =  new ChromeDriver();
-        logger.info("Драйвер начал работать");
     }
-    @Test
-    public void openPage() {
-        driver.get("https://otus.ru/");
-        logger.info("Открыта страница отус");
+    @Before
+    public void  setUp() { driver = new ChromeDriver();}
+
+
+        @After
+        public void setDown () {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+        @Test
+    public void imilic() {
+        driver.get("https://market.yandex.ru");
+        driver.manage().timeouts().implicitlyWait ( 4, TimeUnit.SECONDS );
+
+        driver.findElement(By.xpath());
     }
 
-    @After
-    public void setDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+
+
 
 }
+
